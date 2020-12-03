cf9807af90fccc17dc9fec8197c9f6792fe1f20b
Alfrederson <aroldorosenberg@gmail.com>
2020-12-02 23:20:04 -0300

    Tirei 40% das linhas

--- gambusiavm.js
+++ gambusiavm.js
@@ -1,26 +1,13 @@
-let stack = [0,0,0,0,0,0,0,0,0,0];
-let stack_top = -1;
-
-const push = valor => {
-	stack_top ++;
-  stack[stack_top] = valor;
-}
-
-const pop = () =>{
-	stack_top --;
-  return stack[stack_top+1];
-}
-
-const peek = () =>{
-	return stack[stack_top];
-}
-const set = val =>{
-	stack[stack_top] = val;
-}
-
-const 
-
+const
+stack_size = 256,
+push = valor => stack[--stack_top] = valor,
+pop = () => stack[++stack_top-1],
+peek = () => stack[stack_top],
+set  = val => stack[stack_top] = val,
+heap = p => stack[p],
+store = (p,v) => stack[p]=v,
 opcodes = {
+      NOP  : 0,
 			PUSH : 1,
       ADD  : 2,
       SUB  : 3,
@@ -29,107 +16,59 @@ opcodes = {
       OUT  : 6,
       NEG  : 7,
       NOT  : 8,
+      SET  : 9,
+      LOAD : 10,
+      CALL : 11
+},
+OPCODE = 0,
+ARG    = 1
+functions = {
+	print : () => document.write( `${pop()} <br/>` )
 }
 
-
-      
-      
-const OPCODE = 0,
-      ARG    = 1;
-/*      
-// programa gerado pelo compilador.
-Não sei como fazer isso funcionar ainda.
-2
-4
-5
-MUL
+let stack = new Array(stack_size),
+    stack_top = 256,
+asm = 
+`PUSH 90
+PUSH 30
 ADD
-3
-ADD
-STORE x
-*/
+CALL print
+PUSH Hello
+CALL print
+PUSH World
+CALL print
+`,
+program = []
 
-let asm = 
-`PUSH 9
-NEG
-PUSH 5
-MUL
-OUT`
-
-
-
-
-
-let program = [] /*[
-	[PUSH,2],
-  [PUSH,4],
-  [PUSH,5],
-  [MUL],
-  [ADD],
-  [PUSH,3],
-  [ADD],
-  [OUT]
-]*/
-
-let linhas = asm.split("\n");
-
-linhas.forEach( linha => {
-	linha = linha.trim().split(" ")
-  linha[0] = opcodes[linha[0]]
-  if(linha.length == 2) linha[1] = parseInt(linha[1])
-	program.push( linha)
+asm.split("\n").forEach( l => {
+	l = l.trim().split(";")[0].split(" ")
+  l[0] = opcodes[l[0]]
+  if(l.length == 2){
+  	let num = parseInt(l[1])
+    if(!isNaN(num)) l[1] = num
+  } 
+	program.push(l)
 })
 
-console.log(program);
+let program_counter = 0,
+    program_size    = program.length
 
-let program_counter = 0;
-let program_size    = program.length;
+console.log(`Program has ${program.length} instructions`)
 
-console.log("Programa tem "+program_size+" instruções");
-
-let instrucoes_executadas = 0;
-
-while(program_counter < program_size){
-	let a,b;
-	//console.log(stack);
-	switch( program[program_counter][OPCODE] ){
-    // empurra o argumento na pilha
-  	case opcodes.PUSH:
-    	push(program[program_counter][ARG]);
-    break;
-    case opcodes.ADD:
-    	// pega os dois do alto da pilha, soma, coloca no lugar do segundo e faz um pop.
-      a = pop();
-      b = pop();
-      push ( a + b);
-    break;
-    case opcodes.SUB:
-    	a = pop()
-      b = pop()
-      push ( a - b);
-    case opcodes.MUL:
-      a = pop();
-      b = pop();
-      push ( a * b);    
-    break;
-    case opcodes.DIV:
-    	a = pop()
-      b = pop()
-      push ( a / b);
-    break;
-    case opcodes.OUT:
-    	// Exibe o que estiver no alto da pilha e faz um pop.
-      console.log("OUT-- "+pop());
-    break;
-    case opcodes.NEG:
-    	set( -peek() );
-    break;
-    case opcodes.NOT:
-    	set( peek() == 0 ? 1 : 0 );
-    break;
-  }
-	program_counter++;
-	instrucoes_executadas++;
+const instructions = {
+	[opcodes.PUSH]: () => push(program[program_counter][ARG]),
+  [opcodes.ADD ]: () => push ( pop() + pop() ),
+  [opcodes.SUB ]: () => push ( pop() - pop() ),
+  [opcodes.MUL ]: () => push ( pop() * pop() ),
+  [opcodes.DIV ]: () => push ( pop() / pop() ),
+  [opcodes.NEG ]: () => set  ( -peek() ),
+  [opcodes.NOT ]: () => set  ( peek() === 0 ? 1 : 0 ),
+  [opcodes.LOAD]: () => push ( heap(program[program_counter][ARG]) ),
+  [opcodes.SET ]: () => store(program[program_counter][ARG], pop() ),
+  [opcodes.CALL]: () => functions[program[program_counter][ARG]]() 
 }
 
-console.log(instrucoes_executadas + " instruções executadas.");
+while(program_counter < program_size-1){
+  instructions[ program[program_counter][OPCODE] ]()
+	program_counter++
+}
