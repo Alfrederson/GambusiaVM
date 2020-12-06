834cf45fef0bd9b2db636e1d707241ef17678630
Alfrederson <aroldorosenberg@gmail.com>
2020-12-06 16:46:56 -0300

    Jumps

--- gambusiavm.js
+++ gambusiavm.js
@@ -1,27 +1,20 @@
+const opnames = 
+"NOP,PUSH,ADD,SUB,DIV,MUL,EQL,DIFF,OR,AND,LT,LTE,GT,GTE,NEG,NOT,SET,LOAD,CALL,JMP,JZ,END";
+
+var counter = 0,
+    oc = {}    
+opnames.split(",").forEach( v => oc[v] = counter++ )
+
 const
 stack_size = 256,
 push = valor => stack[--stack_top] = valor,
-pop = () => stack[++stack_top-1],
+pop = () => stack[stack_top++],
 peek = () => stack[stack_top],
 set  = val => stack[stack_top] = val,
 heap = p => stack[p],
 store = (p,v) => stack[p]=v,
-opcodes = {
-      NOP  : 0,
-			PUSH : 1,
-      ADD  : 2,
-      SUB  : 3,
-      DIV  : 4,
-      MUL  : 5,
-      OUT  : 6,
-      NEG  : 7,
-      NOT  : 8,
-      SET  : 9,
-      LOAD : 10,
-      CALL : 11
-},
 OPCODE = 0,
-ARG    = 1
+ARG    = 1,
 functions = {
 	print : () => document.write( `${pop()} <br/>` )
 }
@@ -29,20 +22,29 @@ functions = {
 let stack = new Array(stack_size),
     stack_top = 256,
 asm = 
-`PUSH 90
-PUSH 30
-ADD
-CALL print
-PUSH Hello
-CALL print
-PUSH World
-CALL print
+`
+0:PUSH 0
+1:SET 14    ;   x
+2:LOAD 14 ; loop_start_1
+3:PUSH 13
+4:LT
+5:JZ 13 ; loop_end_1
+6:LOAD 14
+7:CALL print
+8:LOAD 14
+9:PUSH 3
+10:ADD
+11:SET 14    ;   x
+12:JMP 2 ; loop_start_1
+13:END ; loop_end_1
 `,
 program = []
 
 asm.split("\n").forEach( l => {
-	l = l.trim().split(";")[0].split(" ")
-  l[0] = opcodes[l[0]]
+	if(l === "") return
+	l = l.split(":")[1].split(";")[0].trim().split(" ")
+  console.log(l)
+  l[0] = oc[l[0]]
   if(l.length == 2){
   	let num = parseInt(l[1])
     if(!isNaN(num)) l[1] = num
@@ -51,24 +53,40 @@ asm.split("\n").forEach( l => {
 })
 
 let program_counter = 0,
-    program_size    = program.length
+    program_size    = program.length,
+    program_done = false
 
 console.log(`Program has ${program.length} instructions`)
+const arg = qual => program[program_counter][ARG + qual]
 
 const instructions = {
-	[opcodes.PUSH]: () => push(program[program_counter][ARG]),
-  [opcodes.ADD ]: () => push ( pop() + pop() ),
-  [opcodes.SUB ]: () => push ( pop() - pop() ),
-  [opcodes.MUL ]: () => push ( pop() * pop() ),
-  [opcodes.DIV ]: () => push ( pop() / pop() ),
-  [opcodes.NEG ]: () => set  ( -peek() ),
-  [opcodes.NOT ]: () => set  ( peek() === 0 ? 1 : 0 ),
-  [opcodes.LOAD]: () => push ( heap(program[program_counter][ARG]) ),
-  [opcodes.SET ]: () => store(program[program_counter][ARG], pop() ),
-  [opcodes.CALL]: () => functions[program[program_counter][ARG]]() 
+	[oc.PUSH]: () => push(program[program_counter][ARG]),
+  [oc.ADD ]: () => push ( pop() + pop() ),
+  [oc.SUB ]: () => push ( pop() - pop() ),
+  [oc.MUL ]: () => push ( pop() * pop() ),
+  [oc.DIV ]: () => push ( pop() / pop() ),
+  [oc.NEG ]: () => set  ( -peek() ),
+  [oc.NOT ]: () => set  ( peek() === 0 ? 1 : 0 ),
+  [oc.LOAD]: () => push ( heap(arg(0)) ),
+  [oc.SET ]: () => store( arg(0), pop() ),
+  [oc.CALL]: () => functions[arg(0)]()  ,
+  [oc.LT  ]: () => push ( pop() < pop() ? 0 : 1),
+  [oc.GT]  : () => push ( pop() > pop() ? 0 : 1),
+  [oc.LTE] : () => push ( pop() <= pop() ? 0 : 1),
+  [oc.GTE] : () => push ( pop() >= pop() ? 0 : 1),
+  [oc.JZ  ]: () => {
+  	if ( pop() === 0 ) program_counter = arg(0)-1
+  },
+  [oc.JMP]: () => {program_counter = arg(0)-1 },
+  [oc.END]: () =>{
+  	program_done = true
+  }
 }
 
-while(program_counter < program_size-1){
+while(program_counter < program_size){
   instructions[ program[program_counter][OPCODE] ]()
 	program_counter++
+  if(program_done) break
 }
+
+document.write("Done!</br>")
